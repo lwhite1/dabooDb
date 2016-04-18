9de77bb7605b5e83261d64d0f1d94ecc6f111fb4
Spencer Marks <spencer.marks@claritasgenomics.com>
2016-04-18 11:06:51 -0400

    first pass at adding logging interface. this should work for basic logging. Log4j non default config needs to be added. Also unit tests. Wanted to push this out to verify it is the direction you want to go in. The loggingFactory could create the adaptor class to use via configuration when we have more than adaptor pattern for the logger. However, how to used get that properties into the factory is an open issue. I think you'd want it to be configured by either client or server (client and server could use different logging frameworks if they wanted to, but figuring how the factory knows if it is client or server needs to be figured out.
    existing tests pass.

---
+++ src/main/java/org/dabudb/dabu/shared/util/logging/Log4jLogger.java
@@ -0,0 +1,58 @@
+package org.dabudb.dabu.shared.util.logging;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Adaptor for Log4j v2 - wraps our logging interface around
+ * org.apache.logging.log4j
+ */
+public class Log4jLogger implements LoggerWriter {
+
+    private static Logger log4jLogger;
+
+    /**
+     * Create a new logger for the specified class.
+     *
+     * @param clientClass - the class that wants do the logging
+     */
+    Log4jLogger(Class clientClass) {
+        log4jLogger = LogManager.getLogger(clientClass);
+    }
+
+    @Override
+    public void logError(String message, Exception e) {
+        log4jLogger.log(Level.ERROR,message,e);
+    }
+
+    @Override
+    public void logError(String message) {
+        log4jLogger.log(Level.ERROR,message);
+    }
+
+    @Override
+    public void logWarning(String message, Exception e) {
+        log4jLogger.log(Level.WARN,message,e);
+    }
+
+    @Override
+    public void logWarning(String message) {
+        log4jLogger.log(Level.WARN,message);
+    }
+
+    @Override
+    public void logInfo(String message) {
+        log4jLogger.log(Level.INFO,message);
+    }
+
+    @Override
+    public void logDebug(String message, Exception e) {
+        log4jLogger.log(Level.DEBUG,message,e);
+    }
+
+    @Override
+    public void logDebug(String message) {
+        log4jLogger.log(Level.DEBUG,message);
+    }
+}
---
+++ src/main/java/org/dabudb/dabu/shared/util/logging/LoggingFactory.java
@@ -0,0 +1,35 @@
+package org.dabudb.dabu.shared.util.logging;
+
+/**
+ * Clients use this factory to get a specific logger.
+ * The logger compiles to the Logger interface
+ * but it's concrete implementation is defined by configuration
+ * where that implementation could be:
+ *
+ * SLF4j
+ * Logback
+ * Apache log4j 1.2
+ * Apache log4j 2
+ * Java Util Logging
+ * Apache Common Logging
+ *
+ * or any other logging framework. That logging framework is wrapped
+ * using the adaptor pattern allowing any logging framework to be easily
+ * used.
+ */
+public class LoggingFactory {
+
+    /**
+     * Get the logger for a particular class
+     *
+     * @param clientClass the class that wants to do logging.
+     * @return a Logger implementation
+     */
+    public static LoggerWriter getLogger(Class clientClass) {
+        return new Log4jLogger(clientClass);
+
+    }
+
+
+
+}
