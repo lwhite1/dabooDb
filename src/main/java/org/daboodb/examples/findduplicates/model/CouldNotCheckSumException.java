e6b4fc89ccc8bd8ab9524c84667d4b3a32137e83
Spencer Marks <spencer.marks@claritasgenomics.com>
2016-04-22 10:58:09 -0400

    revise file walker approach using simpler inner class

--- src/main/java/org/daboodb/examples/findduplicates/model/CouldNotCheckSumException.java
+++ src/main/java/org/daboodb/examples/findduplicates/model/CouldNotCheckSumException.java
@@ -3,5 +3,15 @@ package org.daboodb.examples.findduplicates.model;
 /**
  *
  */
-public class CouldNotCheckSumException {
+
+public class CouldNotCheckSumException extends RuntimeException {
+
+    public CouldNotCheckSumException(String s) {
+        super(s);
+    }
+
+    public CouldNotCheckSumException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+
 }
